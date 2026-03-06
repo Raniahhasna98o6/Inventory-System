0b18fb62656f1816d8a83236eec70e0dd322e69a
Raniahhasna98o6 <raniahhf@gmail.com>
2026-03-06 17:08:14 +0700

    first commit

---
+++ src/main/java/com/mycompany/inventory_system/Inventory_system.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.inventory_system;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Inventory_system {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
